762b378bf5bfd41e4ccffcb184360b1d35dd3ce1
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-10 12:05:39 +0100

    memegenerator

---
+++ src/components/Meme.jsx
@@ -0,0 +1,24 @@
+import memesData from '../data/memesData';
+
+export default function Meme(){
+
+    function handleClick(){
+        //const arrayMemes = memesData.data.memes;
+        const memeIndex = Math.floor(Math.random() * memesData.data.memes.length);
+        // const meme = memesData[memeIndex];
+        // console.log(meme);
+        alert(memesData.data.memes[memeIndex].name);
+    }
+
+
+
+    return(
+        <main>
+            <div className="form">
+                <input type="text" className="form--input" placeholder="Top text"/>
+                <input type="text" className="form--input" placeholder="Bottom text"/>
+                <button className="form--button" onClick={handleClick}>Get a new meme image 🖼️</button>
+            </div>
+        </main>
+    )
+}
