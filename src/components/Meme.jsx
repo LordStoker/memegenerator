1f4f60e7778ce7e849e693b81ff34c2d1ea55b2e
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-10 21:13:37 +0100

    state random meme

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,14 +1,16 @@
 import memesData from '../data/memesData';
+import {useState} from 'react';
 
 export default function Meme(){
-
-    function handleClick(){
+    const[memeURL, setMemeURL] = useState('');
+    function newMeme(){
         //const arrayMemes = memesData.data.memes;
         const memeIndex = Math.floor(Math.random() * memesData.data.memes.length);
+        setMemeURL(memesData.data.memes[memeIndex].url);
         // const meme = memesData[memeIndex];
         // console.log(meme);
-        const randomMeme = (memesData.data.memes[memeIndex].url);
-        console.log(randomMeme);
+        // const randomMeme = (memesData.data.memes[memeIndex].url);
+        // console.log(randomMeme);
     }
 
 
@@ -18,8 +20,9 @@ export default function Meme(){
             <div className="form">
                 <input type="text" className="form--input" placeholder="Top text"/>
                 <input type="text" className="form--input" placeholder="Bottom text"/>
-                <button className="form--button" onClick={handleClick}>Get a new meme image 🖼️</button>
+                <button className="form--button" onClick={newMeme}>Get a new meme image 🖼️</button>
             </div>
+            <img src={memeURL}></img>
         </main>
     )
 }
