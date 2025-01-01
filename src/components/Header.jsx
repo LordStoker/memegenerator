0f0ae300a837411028a26aebbec1aff42b96aaf6
Lord Stoker <tpc00@iesemilidarder.com>
2025-01-01 01:01:54 +0100

    fetching api with useEffect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,7 @@ export default function Header() {
     return (
         <header className = "header">
             <img className="header--image" src="images/troll-face.png" alt="Problem?" />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h1 className="header--title">Meme Generator</h1>
         </header>
     )
 
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,41 +1,53 @@
-import memesData from '../data/memesData';
+// import memesData from '../data/memesData';
 import {useState} from 'react';
-
+import {useEffect} from 'react';
 export default function Meme(){
     const[meme, setMeme] = useState({
-        topText: '',
-        bottomText: '',
+        topText: 'One does not simply',
+        bottomText: 'Walks into Mordor',
         randomImage: "http://i.imgflip.com/1bij.jpg"   
     });
+    const [allMemeImages, setAllMemeImages] = useState([]);
 
-    const [allMemeImages, setAllMemeImages] = useState(memesData);
-
-    function getMemeImage(){
-        //const arrayMemes = memesData.data.memes;
-        const memeIndex = Math.floor(Math.random() * allMemeImages.data.memes.length);
-        setMeme( prevMemeImg => {
-            return{
-                ...prevMemeImg,
-                 randomImage: allMemeImages.data.memes[memeIndex].url
-                }
-            }
-        );
+    // function getMemeImage(){
+    //     //const arrayMemes = memesData.data.memes;
+    //     const memeIndex = Math.floor(Math.random() * allMemeImages.data.memes.length);
+    //     setMeme( prevMemeImg => {
+    //         return{
+    //             ...prevMemeImg,
+    //              randomImage: allMemeImages.data.memes[memeIndex].url
+    //             }
+    //         }
+    //     );
+    useEffect(() => {
+        fetch('https://api.imgflip.com/get_memes')
+        .then(response => response.json())
+        .then(memesImages => setAllMemeImages(memesImages.data.memes));
+    },[]);
         // const meme = memesData[memeIndex];
         // console.log(meme);
         // const randomMeme = (memesData.data.memes[memeIndex].url);
         // console.log(randomMeme);
-    }
-
-
-
     return(
         <main>
             <div className="form">
-                <input type="text" className="form--input" placeholder="Top text"/>
-                <input type="text" className="form--input" placeholder="Bottom text"/>
-                <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼️</button>
+                <label>Top text
+                    <input type="text" placeholder="Top text" value={meme.topText} onChange={(e) => setMeme(prevMeme => ({...prevMeme, topText: e.target.value}))} />
+                </label>
+                <label>Bottom text
+                    <input type="text" className="form--input" value={meme.bottomText}placeholder="Bottom text" onChange={(e) =>
+                     setMeme(prevMeme =>
+                         ({...prevMeme, 
+                        bottomText: e.target.value
+                    }))}/>
+                </label>
+                <button >Get a new meme image 🖼️</button>
             </div>
-            <img src={meme.randomImage} alt="meme" className='meme--image'></img>
+            <div className="meme">
+                <img src={meme.randomImage} alt="meme" className='meme--image'></img>
+                <span className='top'>{meme.topText}</span>
+                <span className='bottom'>{meme.bottomText}</span>
+            </div>
         </main>
     )
 }
